73dd8531dbeb16bfba99f9031e50e3deccbb17cb
Casperfeng <casp.feng@gmail.com>
2019-01-12 00:08:17 +0100

    calculate-function finished

---
+++ app.js
@@ -0,0 +1,41 @@
+// Listen for submit
+document.getElementById("loan-form").addEventListener("submit", calculateResults);
+
+// Calculate Results
+function calculateResults(e){
+  
+  // UI Vars
+  const amount = document.getElementById("amount");
+  const interest = document.getElementById("interest");
+  const years = document.getElementById("years");
+  const monthlyPayment = document.getElementById("total-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value)/100/12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal*x*calculatedInterest)/(x-1);
+
+  if (isFinite(monthly)){
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+  } else {
+    showError("Please check your numbers");
+  }
+
+  e.preventDefault();
+}
+
+// Show Error
+function showError(error){
+  // Create a div
+  const errorDiv = document.createElement("div");
+  
+  // Add class
+  
+}
